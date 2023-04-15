e3286d9dbf988403924dff08c89ee357e03d6103
Nathan Barnes <npbarnes@alaska.edu>
2023-04-15 15:29:19 -0800

    Second attempt at an analytic solution

--- src/analytic2.jl
+++ src/analytic2.jl
@@ -14,6 +14,7 @@ struct R
     vy0::Float64
     vz0::Float64
 end
+R(x,v) = R(x..., v...)
 
 struct Ξ
     x::Float64
@@ -23,6 +24,13 @@ struct Ξ
     vy::Float64
     vz::Float64
 end
+Ξ(x,v) = Ξ(x..., v...)
+
+position(ξ::Ξ) = SA[ξ.x, ξ.y, ξ.z]
+position(r::R) = SA[r.x0, r.y0, r.z0]
+
+velocity(ξ::Ξ) = SA[ξ.vx, ξ.vy, ξ.vz]
+velocity(r::R) = SA[r.vx0, r.vy0, r.vz0]
 
 function g3(ζ, σ)
     (2π*σ)^(-3/2) * exp(-1/2 * (ζ⋅ζ/σ^2))
@@ -106,6 +114,28 @@ function r(ξ, t)
         vy0(ξ,t),
         vz0(ξ,t)
     )
+end
+
+struct Gaussian
+    μ::SVector{3,Float64}
+    σ::Float64
+end
+function (g::Gaussian)(q)
+    @unpack μ, σ = g
+    (1/(2π*σ^2))^(3/2) * exp(-1/2 * (x-μ)⋅(x-μ)/σ^2)
+end
+
+P6(ξ::Ξ) = P6(position(ξ), velocity(ξ))
+function P6(x,v; v_rocket)
+    x_gauss = Gaussian(zeros(3), 0.1)
+    v_gauss = Gaussian(SA[v_rocket, 0.0, 0.0], dσ_kinetx)
+
+    x_gauss(x) * v_gauss(v)
+end
+
+function fn(t,ξ; Nn)
+    ξ2 = Ξ(position(ξ) - t*velocity(ξ), velocity(ξ))
+    Nn(t) * P6(ξ2)
 end
 
 function h(r,s)
